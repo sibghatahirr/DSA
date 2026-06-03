caf068edb5bf8987bbded1bfad71acf53154bdd6
l1f24bsse0299 <sibghatahir122@gmail.com>
2026-06-03 13:01:39 +0500

    main

--- lab/src/Main.java
+++ lab/src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("program started");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        task1 library = new task1();
+
+        library.addBook("Java Programming");
+        library.addBook("Data Structures");
+        library.addBook("Operating Systems");
+        library.addBook("Algorithms");
+        library.addBook("Java Programming"); // Duplicate
+
+        library.displayAllBooks();
+
+        System.out.println("\nAvailable: " +
+                library.isBookAvailable("Algorithms"));
+
+        library.removeBook("Operating Systems");
+
+        System.out.println();
+        library.displayAllBooks();
+
+        System.out.println();
+        library.displayBooksStartingWith('J');
     }
 }
